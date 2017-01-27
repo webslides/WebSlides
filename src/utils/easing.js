049d0837440b0204db9debfd29bb3fccb1984e37
Antonio Laguna <sombragriselros@gmail.com>
2017-01-27 15:34:34 +0100

    Easing’s JSDoc

--- src/utils/easing.js
+++ src/utils/easing.js
@@ -1,7 +1,17 @@
+/**
+ * Swing easing function.
+ * @param {number} p The percentage of time that has passed.
+ * @return {number}
+ */
 function swing (p) {
   return 0.5 - Math.cos(p * Math.PI) / 2;
 }
 
+/**
+ * Linear easing function.
+ * @param {number} p The percentage of time that has passed.
+ * @return {number}
+ */
 function linear(p) {
   return p;
 }
