ff3c2d066a48676b38aa6f21b1baf9c59ce14894
Antonio Laguna <sombragriselros@gmail.com>
2017-01-29 11:06:22 +0100

    Adding touch plugin

--- src/full.js
+++ src/full.js
@@ -1,6 +1,8 @@
 import WebSlides from './modules/webslides';
 import Scroll from './plugins/scroll';
+import Touch from './plugins/touch';
 
 WebSlides.registerPlugin('Scroll', Scroll);
+WebSlides.registerPlugin('Touch', Touch);
 
 window.WebSlides = WebSlides;
---
+++ src/utils/mobile-detector.js
@@ -0,0 +1,64 @@
+const UA = window.navigator.userAgent;
+
+export default class MobileDetector {
+  /**
+   * Whether the device is Android or not.
+   * @return {Boolean}
+   */
+  static isAndroid() {
+    return !!UA.match(/Android/i);
+  }
+
+  /**
+   * Whether the device is BlackBerry or not.
+   * @return {Boolean}
+   */
+  static isBlackBerry() {
+    return !!UA.match(/BlackBerry/i);
+  }
+
+  /**
+   * Whether the device is iOS or not.
+   * @return {Boolean}
+   */
+  static isiOS() {
+    return !!UA.match(/iPhone/i);
+  }
+
+  /**
+   * Whether the device is Opera or not.
+   * @return {Boolean}
+   */
+  static isOpera() {
+    return !!UA.match(/Opera Mini/i);
+  }
+
+  /**
+   * Whether the device is Windows or not.
+   * @return {Boolean}
+   */
+  static isWindows() {
+    return !!UA.match(/IEMobile/i);
+  }
+
+  /**
+   * Whether the device is Windows Phone or not.
+   * @return {Boolean}
+   */
+  static isWindowsPhone() {
+    return !!UA.match(/Windows Phone/i);
+  }
+
+  /**
+   * Whether the device is any mobile device or not.
+   * @return {Boolean}
+   */
+  static isAny() {
+    return MobileDetector.isAndroid() ||
+    MobileDetector.isBlackBerry() ||
+    MobileDetector.isiOS() ||
+    MobileDetector.isOpera() ||
+    MobileDetector.isWindows() ||
+    MobileDetector.isWindowsPhone();
+  }
+}
