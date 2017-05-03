65a4a47f222433110bba0b749b1c5f0f9b68f076
displaynone <luisitosp@gmail.com>
2017-05-03 23:52:05 +0200

    New test: click nav

---
+++ test/utils/click-nav.test.js
@@ -0,0 +1,26 @@
+import DOM from '../../src/js/utils/dom';
+import ClickNav from '../../src/js/plugins/click-nav';
+
+beforeAll(() => {
+  document.body.innerHTML = `<div id="webslides" data-test="test"><p>Text</p></div>`;
+});
+
+test('AutoSlide plugin', () => {
+  const next = jest.fn();
+  const ws = document.getElementById('webslides');
+  // Simulates dataset
+  ws.dataset = {};
+  const webslides = {
+    options: {
+      changeOnClick: true
+    },
+    goNext: next,
+    el: ws
+  };
+
+  expect(next).not.toBeCalled();
+
+  const clicknav = new ClickNav(webslides);
+  DOM.fireEvent(ws, 'click');
+  expect(next.mock.calls.length).toBe(1);
+});
