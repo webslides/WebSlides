ed9c3142b4bbbad4923b4c05f20d9df8359ea665
Antonio Laguna <sombragriselros@gmail.com>
2017-05-29 22:48:44 +0200

    Removing unused code

--- src/js/utils/easing.js
+++ src/js/utils/easing.js
@@ -7,13 +7,4 @@ function swing(p) {
   return 0.5 - Math.cos(p * Math.PI) / 2;
 }
 
-/**
- * Linear easing function.
- * @param {number} p The percentage of time that has passed.
- * @return {number}
- */
-function linear(p) {
-  return p;
-}
-
-export default {swing, linear};
+export default {swing};
