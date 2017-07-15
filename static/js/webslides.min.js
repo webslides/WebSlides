0e0b2b416024c8d6d1ac8c7bd9e0590b344c87ff
Luis <displaynone@gmail.com>
2017-07-15 14:28:44 +0200

    Fixing index responsive slide sizes

--- static/js/webslides.min.js
+++ static/js/webslides.min.js
@@ -1,7 +1,7 @@
 /*!
  * Name: WebSlides
  * Version: 1.3.1
- * Date: 2017-07-14
+ * Date: 2017-07-15
  * Description: Making HTML presentations easy
  * URL: https://github.com/webslides/webslides#readme
  * Credits: @jlantunez, @LuisSacristan, @Belelros
