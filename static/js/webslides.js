cb60cab236cb549fa999fccb51dcb7eabeac2552
Luis <displaynone@gmail.com>
2017-07-24 20:28:44 +0200

    Improving index slidex sizes

--- static/js/webslides.js
+++ static/js/webslides.js
@@ -1,7 +1,7 @@
 /*!
  * Name: WebSlides
  * Version: 1.3.1
- * Date: 2017-07-16
+ * Date: 2017-07-24
  * Description: Making HTML presentations easy
  * URL: https://github.com/webslides/webslides#readme
  * Credits: @jlantunez, @LuisSacristan, @Belelros
