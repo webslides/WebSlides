4462883dc083322311f834b03ed1d10435974e0e
Luis <displaynone@gmail.com>
2017-08-02 11:33:32 +0200

    Fixing gallery css

--- static/js/webslides.min.js
+++ static/js/webslides.min.js
@@ -1,7 +1,7 @@
 /*!
  * Name: WebSlides
  * Version: 1.3.1
- * Date: 2017-08-01
+ * Date: 2017-08-02
  * Description: Making HTML presentations easy
  * URL: https://github.com/webslides/webslides#readme
  * Credits: @jlantunez, @LuisSacristan, @Belelros
