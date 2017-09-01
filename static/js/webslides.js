d33d06520c0f5c15b95ea0d977a5503f76a7c282
Zeke Nierenberg <hampzan09@gmail.com>
2017-09-01 12:39:38 -0500

    removing reference to non-existant file

--- static/js/webslides.js
+++ static/js/webslides.js
@@ -1,7 +1,7 @@
 /*!
  * Name: WebSlides
  * Version: 1.4.1
- * Date: 2017-08-18
+ * Date: 2017-09-01
  * Description: Making HTML presentations easy
  * URL: https://github.com/webslides/webslides#readme
  * Credits: @jlantunez, @LuisSacristan, @Belelros
