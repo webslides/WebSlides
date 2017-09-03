71b08f8d3bbb1be057ed55fc501398e1d7d11b58
Luis Sacristán <displaynone@gmail.com>
2017-09-03 19:30:05 +0200

    Fixing TOC

--- static/js/webslides.min.js
+++ static/js/webslides.min.js
@@ -1,7 +1,7 @@
 /*!
  * Name: WebSlides
  * Version: 1.4.1
- * Date: 2017-08-18
+ * Date: 2017-09-03
  * Description: Making HTML presentations easy
  * URL: https://github.com/webslides/webslides#readme
  * Credits: @jlantunez, @LuisSacristan, @Belelros
