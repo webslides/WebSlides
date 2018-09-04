6954a310ec7f960c5a57acae10e7c9329f7855b0
Chilly Orange <mattijs@chillyorange.com>
2018-09-04 09:29:28 +0700

    Make sure Webpack uses the plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const SmartBannerPlugin = require('smart-banner-webpack-plugin');
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const WriteFilePlugin = require("write-file-webpack-plugin");
 
 const src = path.join(__dirname, 'src');
 const pkg = require('./package.json');
@@ -40,6 +41,7 @@ module.exports = {
     ]
   },
   plugins: [
+    new WriteFilePlugin(),
     new ExtractTextPlugin('../css/webslides.css'),
     new SmartBannerPlugin({
       banner: `Name: WebSlides\nVersion: ${pkg.version}\nDate: ${new Date().toISOString().slice(0,10)}\nDescription: ${pkg.description}\nURL: ${pkg.homepage}\nCredits: @jlantunez, @LuisSacristan, @Belelros`,
